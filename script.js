20c1a96cf18511e773d00efd2fbea06928fddaca
Vishwanauth Persaud <vpersaud1245@gmail.com>
2023-12-06 17:01:25 -0500

    create gameboard

---
+++ script.js
@@ -0,0 +1,14 @@
+const gameboard = (function () {
+  const gameboard = [];
+  const rows = 3;
+  const columns = 3;
+
+  // create 3x3 gameboard
+  for (let i = 0; i < rows; i++) {
+    gameboard[i] = [];
+    for (let j = 0; j < columns; j++) {
+      gameboard[i][j] = " ";
+    }
+  }
+  console.log(gameboard); //TESING: print gameboard
+})();

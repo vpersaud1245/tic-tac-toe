a2c6ffaf87c3fe6ea12772c166327eb292ef9a68
Vishwanauth Persaud <vpersaud1245@gmail.com>
2023-12-07 19:16:39 -0500

    Add game controller

--- script.js
+++ script.js
@@ -25,11 +25,66 @@ const gameboard = (function () {
     }
   };
 
+  const checkForWin = () => {
+    // Check Rows
+    for (let i = 0; i < rows; i++) {
+      let winFoundRow = gameboard[i].every(
+        (mark) => mark != " " && mark === gameboard[i][0]
+      );
+      if (winFoundRow) {
+        let winner = gameboard[i][0];
+        console.log(`${winner} won, found on row ${i + 1}`);
+        return winFoundRow;
+      }
+    }
+    // Check columns
+    let winFoundColumn = false;
+    for (let i = 0; i < rows; i++) {
+      for (let j = 0; j < columns; j++) {
+        if (i > 0 && gameboard[0][j] != " ") {
+          if (
+            gameboard[0][j] === gameboard[1][j] &&
+            gameboard[0][j] === gameboard[2][j]
+          ) {
+            console.log(`Win found on column ${j + 1}`);
+            winFoundColumn = true;
+            return winFoundColumn;
+          }
+        }
+      }
+    }
+    // Check diagonals
+    let winFoundDiagonal = false;
+    if (gameboard[0][0] != " ") {
+      if (
+        gameboard[1][1] === gameboard[0][0] &&
+        gameboard[2][2] == gameboard[0][0]
+      ) {
+        winFoundDiagonal = true;
+        return winFoundDiagonal;
+        console.log("Win found on diagonal");
+      }
+    } else if (gameboard[0][2] != "" && winFoundDiagonal === false) {
+      if (
+        gameboard[0][2] === gameboard[1][1] &&
+        gameboard[0][2] === gameboard[2][0]
+      ) {
+        winFoundDiagonal = true;
+        return winFoundDiagonal;
+        console.log("Win found on diagonal");
+      }
+    }
+  };
+
   const displayGameboard = () => {
     console.log(gameboard);
   };
 
-  return { placeMarker: placeMarker, displayGameboard: displayGameboard };
+  return {
+    placeMarker: placeMarker,
+    displayGameboard: displayGameboard,
+    checkForWin: checkForWin,
+  };
 })();
 
 /* 
@@ -65,3 +120,50 @@ const players = (function () {
     setPlayerName: setPlayerName,
   };
 })();
+
+/*
+GAME CONTROLLER
+*/
+const gameController = (function () {
+  let playerTurn = "X";
+
+  const switchPlayerTurn = () => {
+    if (playerTurn === "X") {
+      playerTurn = "O";
+    } else {
+      playerTurn = "X";
+    }
+  };
+
+  const displayPlayerTurn = () => {
+    console.log(playerTurn);
+  };
+
+  const playRound = () => {
+    let row = prompt("Enter a row");
+    let column = prompt("Enter a column");
+    gameboard.placeMarker(row, column, playerTurn);
+    gameboard.displayGameboard();
+  };
+
+  const playGame = () => {
+    for (let i = 0; i < 9; i++) {
+      playRound();
+      if (gameboard.checkForWin()) {
+        console.log(`${playerTurn} is the winner`);
+        break;
+      } else if (i === 8) {
+        console.log("It's a draw");
+      }
+      switchPlayerTurn();
+    }
+  };
+
+  return {
+    displayPlayerTurn: displayPlayerTurn,
+    switchPlayerTurn: switchPlayerTurn,
+    playGame: playGame,
+  };
+})();
+
+gameController.playGame();
